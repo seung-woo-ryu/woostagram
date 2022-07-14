ff200c6e2763129ef9ad58cef191922d5570f865
seung-woo-ryu <tmddn645@naver.com>
2022-07-14 15:01:11 +0900

    First Commit

---
+++ src/main/java/com/seungwooryu/woostagram/WoostagramApplication.java
@@ -0,0 +1,13 @@
+package com.seungwooryu.woostagram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WoostagramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WoostagramApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/seungwooryu/woostagram/WoostagramApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seungwooryu.woostagram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WoostagramApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
