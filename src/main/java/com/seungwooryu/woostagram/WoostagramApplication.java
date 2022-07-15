9c0cab85be44fa8fffc296b871789eb41b89727f
seung-woo-ryu <tmddn645@naver.com>
2022-07-15 17:46:05 +0900

    remove: application 파일 변수 사용하는 test 제거, main_app에 어노테이션 제거C

--- src/main/java/com/seungwooryu/woostagram/WoostagramApplication.java
+++ src/main/java/com/seungwooryu/woostagram/WoostagramApplication.java
@@ -1,6 +1,5 @@
 package com.seungwooryu.woostagram;
 
-import lombok.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
