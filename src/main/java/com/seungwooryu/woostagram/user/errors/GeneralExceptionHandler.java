e0f5776e00893939a9908b8d272af698a8cccc35
seung-woo-ryu <tmddn645@naver.com>
2022-07-24 20:17:23 +0900

    add: Exception 추가. Handler에 추가된 exception 등록

--- src/main/java/com/seungwooryu/woostagram/user/errors/GeneralExceptionHandler.java
+++ src/main/java/com/seungwooryu/woostagram/user/errors/GeneralExceptionHandler.java
@@ -1,11 +1,9 @@
 package com.seungwooryu.woostagram.user.errors;
 
-import com.seungwooryu.woostagram.common.utils.ApiUtils;
 import com.seungwooryu.woostagram.user.controller.UserController;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -13,7 +11,6 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import java.util.List;
 
 import static com.seungwooryu.woostagram.common.utils.ApiUtils.*;
-import static com.seungwooryu.woostagram.common.utils.ApiUtils.ApiError.*;
 import static java.util.stream.Collectors.toList;
 
 @RestControllerAdvice(basePackageClasses = UserController.class)
@@ -21,10 +18,11 @@ import static java.util.stream.Collectors.toList;
 public class GeneralExceptionHandler {
 
     /**
-     *  javax.validation.Valid or @Validated 으로 binding error 발생시 발생한다.
+     *  javax.validation.Valid or @Validated에 @RequestBody, @RequestPart으로 binding error 발생시 발생한다.
      *  HttpMessageConverter 에서 등록한 HttpMessageConverter binding 못할경우 발생
-     *  주로 @RequestBody, @RequestPart 어노테이션에서 발생
+     *  @ModelAttribute로 바인딩 exception시에는 BindException error 발생.
      */
+
     @ExceptionHandler({MethodArgumentNotValidException.class})
     public ResponseEntity<ApiResult<?>> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
         List<ApiError.FieldError> fieldErrors = e.getBindingResult().getFieldErrors().stream()
@@ -36,4 +34,14 @@ public class GeneralExceptionHandler {
         return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
     }
 
+    @ExceptionHandler({DuplicatedArgumentException.class})
+    public ResponseEntity<ApiResult<?>> handleDuplicatedArgumentException(DuplicatedArgumentException e) {
+        List<ApiError.FieldError> fieldErrors = e.getFieldErrors().stream()
+                .map(ApiError.FieldError::createFieldError)
+                .collect(toList());
+
+        final ApiResult<?> error = error(HttpStatus.BAD_REQUEST.getReasonPhrase(), HttpStatus.BAD_REQUEST, fieldErrors);
+
+        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
+    }
 }
