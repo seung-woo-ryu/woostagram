dc5301954bf01628280e628a056beba1a9074952
seung-woo-ryu <tmddn645@naver.com>
2022-07-24 20:19:19 +0900

    fix: UserController,Service 임시 수정. 회원 가입 성공 완료 후 로직 수정 필요

--- src/main/java/com/seungwooryu/woostagram/user/controller/UserController.java
+++ src/main/java/com/seungwooryu/woostagram/user/controller/UserController.java
@@ -1,20 +1,17 @@
 package com.seungwooryu.woostagram.user.controller;
 
-import com.seungwooryu.woostagram.common.utils.ApiUtils.*;
+import com.seungwooryu.woostagram.common.utils.ApiUtils.ApiResult;
 import com.seungwooryu.woostagram.user.dto.SignupDto;
 import com.seungwooryu.woostagram.user.service.UserService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.HttpClientErrorException;
 
 import javax.validation.Valid;
 import java.util.ArrayList;
-import java.util.List;
 
 import static com.seungwooryu.woostagram.common.utils.ApiUtils.success;
 
@@ -27,9 +24,11 @@ public class UserController {
     }
 
     @PostMapping("/signup")
-    public ResponseEntity<ApiResult<?>> signup(@Valid @RequestBody SignupDto signupDto) {
-        // 원래 쿠키,세션
+    public ResponseEntity<ApiResult<?>> signupUser(@Valid @RequestBody SignupDto signupDto) {
+        // 회원가입 성공한 유저 정보로 /login으로 포워딩.
         final ApiResult<String> response = success(new ArrayList<>());
+
+        userService.signupUser(signupDto);
 
         return new ResponseEntity<>(response, HttpStatus.OK);
     }
--- src/main/java/com/seungwooryu/woostagram/user/service/UserService.java
+++ src/main/java/com/seungwooryu/woostagram/user/service/UserService.java
@@ -1,7 +1,56 @@
 package com.seungwooryu.woostagram.user.service;
 
+import com.seungwooryu.woostagram.user.domain.User;
+import com.seungwooryu.woostagram.user.dto.SignupDto;
+import com.seungwooryu.woostagram.user.errors.DuplicatedArgumentException;
+import com.seungwooryu.woostagram.user.repository.UserRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.validation.FieldError;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
+@Slf4j
 public class UserService {
+    UserRepository userRepository;
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Transactional(readOnly=true)
+    public boolean checkEmailDuplicate(String email) {return userRepository.existsByEmail(email);}
+    @Transactional(readOnly=true)
+    public boolean checkNicknameDuplicate(String nickname) {return userRepository.existsByNickname(nickname);}
+
+    @Transactional
+    public User saveUser(SignupDto signupDto) {
+        User newUser = User.createUserBySignupDto(signupDto);
+        return userRepository.save(newUser);
+    }
+
+    public User signupUser(SignupDto signupDto) {
+        List<FieldError> fieldErrors = new ArrayList<>();
+        //이메일 중복
+        if (checkEmailDuplicate(signupDto.getEmail())) {
+            fieldErrors.add(new FieldError("signupDto","email","중복된 이메일입니다"));
+        }
+        //닉네임 중복
+        if (checkNicknameDuplicate(signupDto.getNickname())) {
+            fieldErrors.add(new FieldError("signupDto","nickname","중복된 닉네임입니다"));
+        }
+
+        // 중복된 속성 존재시 exception 발생
+        if (! fieldErrors.isEmpty()) {
+            log.info("Duplicated Errors");
+            throw new DuplicatedArgumentException(fieldErrors);
+        }
+
+        // 중복되지 않은 유저라면 saveUser
+        User savedUser = saveUser(signupDto);
+
+        return savedUser;
+    }
 }
