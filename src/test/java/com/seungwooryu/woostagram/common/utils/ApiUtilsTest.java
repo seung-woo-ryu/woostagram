359ea253de1af77483764a0ce95177ff767d281e
seung-woo-ryu <tmddn645@naver.com>
2022-07-26 16:53:27 +0900

    refactor: test 코드 수정

--- src/test/java/com/seungwooryu/woostagram/common/utils/ApiUtilsTest.java
+++ src/test/java/com/seungwooryu/woostagram/common/utils/ApiUtilsTest.java
@@ -4,9 +4,6 @@ import com.seungwooryu.woostagram.common.utils.ApiUtils.ApiResult;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.ApplicationContext;
 import org.springframework.http.HttpStatus;
 
 import java.util.ArrayList;
@@ -30,28 +27,25 @@ class ApiUtilsTest {
         log.info("ApiResult(success): {}", apiResult);
 
         assertThat(apiResult.isSuccess()).isEqualTo(true);
-        assertThat(apiResult.getResponse()).isEqualTo("json");
+        assertThat(apiResult.getResponse()).isEqualTo(json);
         assertThat(apiResult.getError()).isNull();
     }
 
     @Test
-    @DisplayName("ApiResult 에러")
-    void error() {
+    @DisplayName("ApiResult 에러2")
+    void error2() {
         //given
         var status = HttpStatus.BAD_REQUEST;
-        var throwable = new Throwable(String.valueOf(NullPointerException.class));
 
         //when
-        var apiResult = ApiUtils.error(throwable.getMessage(),status);
+        var apiResult = ApiUtils.error(status.getReasonPhrase(), status);
 
         //then
         log.info("ApiResult(error): {}", apiResult);
 
         assertThat(apiResult.isSuccess()).isEqualTo(false);
         assertThat(apiResult.getResponse()).isNull();
-        assertThat(apiResult.getError().getMessage()).isEqualTo(String.valueOf(NullPointerException.class));
+        assertThat(apiResult.getError().getMessage()).isEqualTo(HttpStatus.BAD_REQUEST.getReasonPhrase());
         assertThat(apiResult.getError().getStatus()).isEqualTo(status.value());
-
-
     }
 }
