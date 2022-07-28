a6fd947b65156df220f3b54c750acbf56ef0d671
seung-woo-ryu <tmddn645@naver.com>
2022-07-29 00:46:39 +0900

    refactor: null check by Optional.ofNullabel()

--- src/main/java/com/seungwooryu/woostagram/user/interceptor/SigninCheckInterceptor.java
+++ src/main/java/com/seungwooryu/woostagram/user/interceptor/SigninCheckInterceptor.java
@@ -6,27 +6,19 @@ import org.springframework.web.servlet.HandlerInterceptor;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
+import java.util.Optional;
 
 @Slf4j
 public class SigninCheckInterceptor implements HandlerInterceptor {
     @Override
-    // 로그인은 preHandle 만 있어도 됨
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         log.debug("URI: {}", request.getRequestURI());
         log.debug("METHOD: {}", request.getMethod());
 
-        /*HttpSession session = Optional.ofNullable(request.getSession(false))
-                .orElseThrow(() -> {
-                    log.info("미인증 사용자 요청");
-                    throw new AuthenticationException();
-                });*/
-        HttpSession session = request.getSession(false);
-
-        if (session == null) {
+        Optional.ofNullable(request.getSession(false)).orElseThrow(() -> {
             log.info("미인증 사용자 요청");
             throw new AuthenticationException();
-        }
+        });
 
         return true;
     }
