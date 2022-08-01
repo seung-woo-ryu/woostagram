859e07fa87ec4dddd9279978864623e6e546d76f
seung-woo-ryu <tmddn645@naver.com>
2022-08-01 15:07:21 +0900

    refactor: AWS S3 config, 파일 업로드, 삭제 기능 추가

---
+++ src/main/java/com/seungwooryu/woostagram/common/utils/Constants.java
@@ -0,0 +1,5 @@
+package com.seungwooryu.woostagram.common.utils;
+
+public final class Constants {
+    public static final String LOGGED_IN_USER_SESSION_KEY = "user";
+}
--- src/main/java/com/seungwooryu/woostagram/post/service/FileService.java
+++ src/main/java/com/seungwooryu/woostagram/post/service/FileService.java
@@ -3,7 +3,9 @@ package com.seungwooryu.woostagram.post.service;
 import org.springframework.web.multipart.MultipartFile;
 
 public interface FileService {
-    public String upload(MultipartFile file);
+    public String upload(MultipartFile file, String folder);
 
-    public Boolean delete(String fileURI);
+    public void delete(String fileURI);
+
+    String generateFileName(String fileName);
 }
