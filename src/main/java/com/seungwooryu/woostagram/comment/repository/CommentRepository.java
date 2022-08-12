7465f3402894e9411abc3c00cc49f9addab41de0
seung-woo-ryu <tmddn645@naver.com>
2022-08-12 11:57:26 +0900

    add: feed관련 repository 기능 추가

--- src/main/java/com/seungwooryu/woostagram/comment/repository/CommentRepository.java
+++ src/main/java/com/seungwooryu/woostagram/comment/repository/CommentRepository.java
@@ -5,7 +5,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.List;
+
 public interface CommentRepository extends JpaRepository<Comment, Long> {
     @Query("select c from Comment c where user.id = :userId and post.id = :postId")
     Comment findByUserIdAndPostId(@Param("userId") Long userId, @Param("postId") Long postId);
+
+    @Query("select c from Comment c where post.id = :postId order by c.createdAt desc")
+    List<Comment> findAllByPostId(@Param("postId") Long postId);
 }
--- src/main/java/com/seungwooryu/woostagram/follow/repository/FollowRepository.java
+++ src/main/java/com/seungwooryu/woostagram/follow/repository/FollowRepository.java
@@ -3,10 +3,13 @@ package com.seungwooryu.woostagram.follow.repository;
 import com.seungwooryu.woostagram.follow.domain.Follow;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface FollowRepository extends JpaRepository<Follow, Long> {
     boolean existsByFromUser_IdAndToUser_Id(Long followerId, Long followedUserId);
 
     Optional<Follow> findByFromUser_IdAndToUser_Id(Long followerId, Long followedUserId);
+
+    List<Follow> findAllByFromUser_Id(Long fromId);
 }
